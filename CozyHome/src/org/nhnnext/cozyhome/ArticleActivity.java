14fe6badf711e7c7e0f794beb6307ed18756c0c1
Yoonsung <lvev9925@naver.com>
2014-11-25 09:09:22 +0900

    Create Write, Article Activity and add Event

---
+++ CozyHome/src/org/nhnnext/cozyhome/ArticleActivity.java
@@ -0,0 +1,15 @@
+package org.nhnnext.cozyhome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArticleActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_article);
+	}
+
+}
--- CozyHome/src/org/nhnnext/cozyhome/MainActivity.java
+++ CozyHome/src/org/nhnnext/cozyhome/MainActivity.java
@@ -6,27 +6,46 @@ import org.nhnnext.cozyhome.model.ListData;
 import org.nhnnext.cozyhome.support.CustomListAdapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnItemClickListener{
+public class MainActivity extends Activity implements OnItemClickListener, OnClickListener{
 
+	private static final String TAG = MainActivity.class.getName();
 	private ArrayList<ListData> dataList = new ArrayList<ListData>(); 
+	private Button btnWrite;
+	private Button btnRefresh;
+	private ListView listView;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_main);
 	    
-	    ListView listView = (ListView) findViewById(R.id.main_list);
+	    initialize();
+	    setupListView();
+	    setEvent();
 	    
-	    ListData dto = null;
+	}
+
+	private void setEvent() {
+		listView.setOnItemClickListener(this);
+		btnRefresh.setOnClickListener(this);
+		btnWrite.setOnClickListener(this);
+	}
+
+	private void setupListView() {
+		ListData dto = null;
 	    for (int i = 0; i < 5 ; i++) {
+	    	//TODO DELTE Codeline Text
 	    	dto = new ListData(i+"-첫번째 줄", i+"-두번째 줄", "0"+i+".jpg");
 	    	dataList.add(dto);
 		}
@@ -34,14 +53,35 @@ public class MainActivity extends Activity implements OnItemClickListener{
 	    CustomListAdapter adapter = new CustomListAdapter(this, R.layout.main_list_row, dataList);
 	    
 	    listView.setAdapter(adapter);
-	    listView.setOnItemClickListener(this);
+	}
+
+	private void initialize() {
+		listView = (ListView) findViewById(R.id.main_list);
+	    btnWrite = (Button) findViewById(R.id.main_btn_write);
+	    btnRefresh = (Button) findViewById(R.id.main_btn_refresh);
 	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		//TODO DELTE Codeline Text
 		String msg = position+ "번 리스트가 선택됨";
-		Log.i("TEST", msg);
+		Log.i(TAG, msg);
 		Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		switch (v.getId()) {
+		case R.id.main_btn_write:
+			startActivity(new Intent(MainActivity.this, WriteActivity.class));
+			break;
+
+		case R.id.main_btn_refresh:
+			//TODO DELTE Codeline Text
+			Toast.makeText(MainActivity.this, "Refresh!", Toast.LENGTH_SHORT).show();
+			break;
+		}
 	}
 
 }
